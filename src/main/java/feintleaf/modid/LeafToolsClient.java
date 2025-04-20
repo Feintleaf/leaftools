e17f03d986d49a49afd84a9152d3490d09c972db
BuildTools <unconfigured@null.spigotmc.org>
2025-04-19 21:40:02 -0700

    Initial commit

---
+++ src/main/java/feintleaf/modid/LeafToolsClient.java
@@ -0,0 +1,10 @@
+package feintleaf.modid;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LeafToolsClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/feintleaf/modid/Leaftools.java
@@ -0,0 +1,15 @@
+package feintleaf.modid;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Leaftools implements ModInitializer {
+	public static final String MOD_ID = "leaftools";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
